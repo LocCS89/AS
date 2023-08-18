458f1093b12e067de662df8faf01aedf29784a91
LocCS89 <93807490+LocCS89@users.noreply.github.com>
2023-08-18 15:00:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,38 +7,40 @@ app.use(bodyParser.json());
 var cors = require('cors')
 app.use(cors())
 
-mongoose.connect('mongodb://0.0.0.0:27017/sensors', {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('MongoDB connected…'))
-    .catch(err => console.log(err))
+mongoose.connect('mongodb://0.0.0.0:27017/sensors', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected…'))
+  .catch(err => console.log(err))
 app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Server started on port 3000'));
 const sensorSchema = new mongoose.Schema({
-    id: String,
-    lat: Number,
-    lng: Number,
-    CO2: Number,
-    dust: Number,
-  });
+  id: String,
+  CO2: Number,
+  dust: Number,
+});
+
 const Sensor = mongoose.model('Sensor', sensorSchema);
 
 app.get('/sensors', (req, res) => {
-    Sensor.find()
-      .then(sensors => res.json(sensors))
-      .catch(err => res.status(500).json({ error: err }));
-  });
-  
-app.post('/sensors', (req, res) => {
-    const newSensor = new Sensor(req.body);
-  
-    newSensor.save()
-      .then(sensor => res.json(sensor))
-      .catch(err => res.status(500).json({ error: err }));
-  });
-  fetch('http://localhost:3000/sensors')
-  .then(response => response.json())
-  .then(sensors => {
-    console.log(sensors);
-  });
+  Sensor.find()
+    .then(sensors => res.json(sensors))
+    .catch(err => res.status(500).json({ error: err }));
+});
 
-  
+app.post('/sensors', (req, res) => {
+  const newSensor = new Sensor(req.body);
+
+  newSensor.save()
+    .then(sensor => res.json(sensor))
+    .catch(err => res.status(500).json({ error: err }));
+});
+app.put('/sensors/:id', (req, res) => {
+  Sensor.findOneAndUpdate({ id: req.params.id }, req.body, { new: true })
+    .then(sensor => res.status(200).json({ msg: "success" }))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+
+
+
+
